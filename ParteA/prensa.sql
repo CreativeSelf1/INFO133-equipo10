5485c280353a32be60b2d4b7b084ce4a735d3644
CreativeSelf1 <fernacastillo.perez0607@gmail.com>
2023-06-15 10:44:26 -0400

    cambios

--- ParteA/prensa.sql
+++ ParteA/prensa.sql
@@ -16,88 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Fundadores`
---
-
-DROP TABLE IF EXISTS `Fundadores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Fundadores` (
-  `PK_ID` int(11) NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `apellido` varchar(255) DEFAULT NULL,
-  `FK_nombre_medioprensa` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`PK_ID`),
-  KEY `FK_nombre_medioprensa` (`FK_nombre_medioprensa`),
-  CONSTRAINT `Fundadores_ibfk_1` FOREIGN KEY (`FK_nombre_medioprensa`) REFERENCES `medios_de_prensa` (`PK_nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Fundadores`
---
-
-LOCK TABLES `Fundadores` WRITE;
-/*!40000 ALTER TABLE `Fundadores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Fundadores` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Noticia`
---
-
-DROP TABLE IF EXISTS `Noticia`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Noticia` (
-  `PK_url_noticia` varchar(255) NOT NULL,
-  `XPATH_fecha` varchar(255) DEFAULT NULL,
-  `XPATH_titulo` varchar(255) DEFAULT NULL,
-  `XPATH_contenido` varchar(255) DEFAULT NULL,
-  `FK_url_categoria` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`PK_url_noticia`),
-  KEY `FK_url_categoria` (`FK_url_categoria`),
-  CONSTRAINT `Noticia_ibfk_1` FOREIGN KEY (`FK_url_categoria`) REFERENCES `categoria` (`PK_url_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Noticia`
---
-
-LOCK TABLES `Noticia` WRITE;
-/*!40000 ALTER TABLE `Noticia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Noticia` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Redes_sociales`
---
-
-DROP TABLE IF EXISTS `Redes_sociales`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Redes_sociales` (
-  `PK_cuenta` varchar(255) NOT NULL,
-  `numero_seguidores` int(11) DEFAULT NULL,
-  `fec_ultima_actualizacion` date DEFAULT NULL,
-  `FK_nombre_medioprensa` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`PK_cuenta`),
-  KEY `FK_nombre_medioprensa` (`FK_nombre_medioprensa`),
-  CONSTRAINT `Redes_sociales_ibfk_1` FOREIGN KEY (`FK_nombre_medioprensa`) REFERENCES `medios_de_prensa` (`PK_nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Redes_sociales`
---
-
-LOCK TABLES `Redes_sociales` WRITE;
-/*!40000 ALTER TABLE `Redes_sociales` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Redes_sociales` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `categoria`
 --
 
@@ -105,13 +23,13 @@ DROP TABLE IF EXISTS `categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria` (
-  `PK_url_categoria` varchar(255) NOT NULL,
+  `url_categoria` varchar(255) NOT NULL,
   `nombre` varchar(255) DEFAULT NULL,
   `XPATH` varchar(255) DEFAULT NULL,
-  `FK_nombre_medioprensa` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`PK_url_categoria`),
-  KEY `FK_nombre_medioprensa` (`FK_nombre_medioprensa`),
-  CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`FK_nombre_medioprensa`) REFERENCES `medios_de_prensa` (`PK_nombre`)
+  `nombre_prensa` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`url_categoria`),
+  KEY `FK_nombre_medioprensa` (`nombre_prensa`),
+  CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`nombre_prensa`) REFERENCES `medios_de_prensa` (`nombre_prensa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -125,6 +43,33 @@ LOCK TABLES `categoria` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `fundadores`
+--
+
+DROP TABLE IF EXISTS `fundadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fundadores` (
+  `ID_fundador` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellido` varchar(255) DEFAULT NULL,
+  `nombre_prensa` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID_fundador`),
+  KEY `FK_nombre_medioprensa` (`nombre_prensa`),
+  CONSTRAINT `fundadores_ibfk_1` FOREIGN KEY (`nombre_prensa`) REFERENCES `medios_de_prensa` (`nombre_prensa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fundadores`
+--
+
+LOCK TABLES `fundadores` WRITE;
+/*!40000 ALTER TABLE `fundadores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fundadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `medios_de_prensa`
 --
 
@@ -132,17 +77,17 @@ DROP TABLE IF EXISTS `medios_de_prensa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `medios_de_prensa` (
-  `PK_nombre` varchar(255) NOT NULL,
+  `nombre_prensa` varchar(255) NOT NULL,
   `año_fundacion` int(11) DEFAULT NULL,
   `cobertura` varchar(255) DEFAULT NULL,
   `url_principal` varchar(255) DEFAULT NULL,
-  `FK_ID_ubicacion` int(11) DEFAULT NULL,
-  `FK_url_noticia` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`PK_nombre`),
-  KEY `FK_ID_ubicacion` (`FK_ID_ubicacion`),
-  KEY `FK_url_noticia` (`FK_url_noticia`),
-  CONSTRAINT `medios_de_prensa_ibfk_1` FOREIGN KEY (`FK_ID_ubicacion`) REFERENCES `ubicacion` (`PK_ID_ubicacion`),
-  CONSTRAINT `medios_de_prensa_ibfk_2` FOREIGN KEY (`FK_url_noticia`) REFERENCES `Noticia` (`PK_url_noticia`)
+  `ID_ubicacion` int(11) DEFAULT NULL,
+  `url_noticia` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`nombre_prensa`),
+  KEY `FK_ID_ubicacion` (`ID_ubicacion`),
+  KEY `FK_url_noticia` (`url_noticia`),
+  CONSTRAINT `medios_de_prensa_ibfk_1` FOREIGN KEY (`ID_ubicacion`) REFERENCES `ubicacion` (`ID_ubicacion`),
+  CONSTRAINT `medios_de_prensa_ibfk_2` FOREIGN KEY (`url_noticia`) REFERENCES `noticia` (`url_noticia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -152,7 +97,63 @@ CREATE TABLE `medios_de_prensa` (
 
 LOCK TABLES `medios_de_prensa` WRITE;
 /*!40000 ALTER TABLE `medios_de_prensa` DISABLE KEYS */;
+INSERT INTO `medios_de_prensa` VALUES ('austral',1990,'nacional','',NULL,NULL),('jupoter',1990,'nacional','https://www.youtube.com/',NULL,NULL),('merc',19090,'nacional','https://www.youtube.com/watch?v=95Qu2xavAsA',NULL,NULL),('mercurio',1990,'nacional','https://www.instagram.com/',NULL,NULL);
 /*!40000 ALTER TABLE `medios_de_prensa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `noticia`
+--
+
+DROP TABLE IF EXISTS `noticia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `noticia` (
+  `url_noticia` varchar(255) NOT NULL,
+  `XPATH_fecha` varchar(255) DEFAULT NULL,
+  `XPATH_titulo` varchar(255) DEFAULT NULL,
+  `XPATH_contenido` varchar(255) DEFAULT NULL,
+  `url_categoria` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`url_noticia`),
+  KEY `FK_url_categoria` (`url_categoria`),
+  CONSTRAINT `noticia_ibfk_1` FOREIGN KEY (`url_categoria`) REFERENCES `categoria` (`url_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `noticia`
+--
+
+LOCK TABLES `noticia` WRITE;
+/*!40000 ALTER TABLE `noticia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `noticia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `redes_sociales`
+--
+
+DROP TABLE IF EXISTS `redes_sociales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `redes_sociales` (
+  `cuenta` varchar(255) NOT NULL,
+  `numero_seguidores` int(11) DEFAULT NULL,
+  `fec_ultima_actualizacion` date DEFAULT NULL,
+  `nombre_prensa` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`cuenta`),
+  KEY `FK_nombre_medioprensa` (`nombre_prensa`),
+  CONSTRAINT `redes_sociales_ibfk_1` FOREIGN KEY (`nombre_prensa`) REFERENCES `medios_de_prensa` (`nombre_prensa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `redes_sociales`
+--
+
+LOCK TABLES `redes_sociales` WRITE;
+/*!40000 ALTER TABLE `redes_sociales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `redes_sociales` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -163,12 +164,12 @@ DROP TABLE IF EXISTS `ubicacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ubicacion` (
-  `PK_ID_ubicacion` int(11) NOT NULL,
+  `ID_ubicacion` int(11) NOT NULL,
   `ciudad` varchar(255) DEFAULT NULL,
   `region` varchar(255) DEFAULT NULL,
   `pais` varchar(255) DEFAULT NULL,
   `continente` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`PK_ID_ubicacion`)
+  PRIMARY KEY (`ID_ubicacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -194,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-14 13:43:21
+-- Dump completed on 2023-06-15 10:26:57
